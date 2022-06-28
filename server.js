da694b0204885d04c3c03fc06abdc8ed1571bc34
Erica Layton <e.alexislayton@gmail.com>
2022-06-28 15:16:44 -0700

    add middleware

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
-app.use(cors())
 
 
 
@@ -20,6 +19,14 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('movies')
     })
+
+app.set('view engine', 'ejs')               // helps us with templating 
+app.use(express.static('public'))           // set up a folder to hold our css, js, fonts, imgs without a link
+app.use(express.urlencoded({ extended:true }))  // helps us parse urls
+app.use(express.json())                     // help express parse json
+app.use(cors())
+
+
 
 
 app.listen(process.env.PORT || PORT, _ => {
